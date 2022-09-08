082b44bdf2d8dd87bdcaa2fd12233b2f512d82f6
test <1169101375@qq.com>
2022-09-08 23:06:32 +0800

    1.0

---
+++ hos/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import axios from 'axios'
+import qs from 'qs'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+Vue.use(ElementUI);
+
+Vue.prototype.$axios=axios 
+Vue.prototype.$qs=qs
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
